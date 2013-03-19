661580f1755eb456f528eaf4c9c316c53ff367ae
clown <tt.clown@gmail.com>
2013-03-19 18:14:45 +0900

    WPF アプリのためのモックを追加

---
+++ CubePdf.Wpf/ImageView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace CubePdf.Wpf
+{
+    public class ImageView : System.Windows.Controls.ViewBase
+    {
+        protected override object DefaultStyleKey
+        {
+            get { return new ComponentResourceKey(GetType(), "ImageView"); }
+        }
+
+        protected override object ItemContainerDefaultStyleKey
+        {
+            get { return new ComponentResourceKey(GetType(), "ImageViewItem"); }
+        }
+    }
+}
