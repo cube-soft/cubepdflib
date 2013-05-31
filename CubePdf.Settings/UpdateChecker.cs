560dac2df3567717f0b5cfa5045b117259904d59
clown <tt.clown@gmail.com>
2013-05-31 17:28:30 +0900

    アップデートチェック用のクラスを作成

---
+++ CubePdf.Settings/UpdateChecker.cs
@@ -0,0 +1,285 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// UpdateChecker.cs
+///
+/// Copyright (c) 2013 CubeSoft, Inc. All rights reserved.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see < http://www.gnu.org/licenses/ >.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace CubePdf.Settings
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// UpdateChecker
+    ///
+    /// <summary>
+    /// www.cube-soft.jp に問い合わせて、バージョンアップが行われているか
+    /// どうかを確認するためのクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class UpdateChecker
+    {
+        #region Initializing and Terminating
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// UpdateChecker (constructor)
+        /// 
+        /// <summary>
+        /// 既定の値でオブジェクトを初期化します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public UpdateChecker() { }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// UpdateChecker (constructor)
+        /// 
+        /// <summary>
+        /// 引数に指定されたサブキー名を利用して、オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public UpdateChecker(string subkey)
+        {
+            InitializeVariables(subkey);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ProductName
+        /// 
+        /// <summary>
+        /// 製品名を取得、または設定します。このプロパティに設定する値は、
+        /// URL の一部として使用されます。したがって、例えば CubePDF の場合、
+        /// 設定すべき値は "cubepdf" となります。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string ProductName
+        {
+            get { return _product; }
+            set { _product = value; }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Version
+        /// 
+        /// <summary>
+        /// 現在インストールされているバージョンを取得、または設定します。
+        /// コンストラクタにレジストリのサブキーを設定した場合は、
+        /// オブジェクトの初期化時にバージョン情報が取得されます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string Version
+        {
+            get { return _version; }
+            set { _version = value; }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// CheckInterval
+        /// 
+        /// <summary>
+        /// バージョンチェックを行うインターバルを取得、または設定します。
+        /// 前回に GetResponse メソッドを実行してから CheckInterval 日
+        /// 経過していない場合、GetResponse メソッドの実行はスキップされます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public int CheckInterval
+        {
+            get { return _interval; }
+            set { _interval = value; }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Notify
+        /// 
+        /// <summary>
+        /// サーバへリクエストを送ります。サーバからのレスポンスは無視され
+        /// ます。このメソッドは、主にインストール直後に使用されます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Notify()
+        {
+            var request = GetRequest("flag=install");
+            using (var response = request.GetResponse())
+            {
+                // do nothing
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetResponse
+        /// 
+        /// <summary>
+        /// サーバへアップデートチェックのためのリクエストを送り、現在の
+        /// バージョンに対応するレスポンスを取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public IDictionary<string, string> GetResponse()
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() ||
+                DateTime.Now <= _last.AddDays(_interval)) return null;
+
+            try
+            {
+                var request = GetRequest("");
+                using (var response = request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                using (var reader = new System.IO.StreamReader(stream, System.Text.Encoding.GetEncoding("UTF-8")))
+                {
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        if (line.Length > 0 && line[0] == '[' && line[line.Length - 1] == ']')
+                        {
+                            var compare = line.Substring(1, line.Length - 2);
+                            if (compare == _version) return ParseResponse(reader);
+                        }
+                        line = reader.ReadLine();
+                    }
+                }
+                return null;
+            }
+            finally
+            {
+                if (!string.IsNullOrEmpty(_subkey))
+                {
+                    var registry = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(_subkey);
+                    registry.SetValue(REG_LASTCHECK, DateTime.Now.ToString());
+                }
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// InitializeVariables
+        /// 
+        /// <summary>
+        /// 引数に指定されたレジストリのサブキーを利用して、メンバ変数を
+        /// 初期化します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void InitializeVariables(string subkey)
+        {
+            try
+            {
+                _subkey = subkey;
+
+                var hklm = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(subkey, false);
+                _version = (string)hklm.GetValue(REG_VERSION, string.Empty);
+                hklm.Close();
+                
+                var hkcu = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(subkey, false);
+                var date = (string)hkcu.GetValue(REG_LASTCHECK, string.Empty);
+                if (string.IsNullOrEmpty(date)) _last = DateTime.Parse(date);
+            }
+            catch (Exception err) { Trace.TraceError(err.ToString()); }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetRequest
+        /// 
+        /// <summary>
+        /// サーバへのリクエストを生成します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private System.Net.WebRequest GetRequest(string suffix)
+        {
+            var url = string.Format("http://{0}/{1}/update.php?ver={2}",
+                Properties.Resources.DomainName, _product, System.Web.HttpUtility.UrlEncode(_version));
+            if (!string.IsNullOrEmpty(suffix)) url += "&" + suffix;
+            return System.Net.WebRequest.Create(url);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ParseResponse
+        /// 
+        /// <summary>
+        /// サーバから取得したレスポンスを解析します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private IDictionary<string, string> ParseResponse(System.IO.StreamReader reader)
+        {
+            var dest = new Dictionary<string, string>();
+            var line = reader.ReadLine();
+            while (line != null)
+            {
+                if (line.Length > 0)
+                {
+                    if (line[0] == '[' && line[line.Length - 1] == ']') break;
+
+                    var pos = line.IndexOf('=');
+                    if (pos >= 0)
+                    {
+                        var key = line.Substring(0, pos);
+                        var value = line.Substring(pos + 1, line.Length - (pos + 1));
+                        if (dest.ContainsKey(key)) dest[key] = value;
+                        else dest.Add(key, value);
+                    }
+                }
+                line = reader.ReadLine();
+            }
+            return dest;
+        }
+
+        #endregion
+
+        #region Variables
+        private string _subkey = string.Empty;
+        private string _product = string.Empty;
+        private string _version = string.Empty;
+        private int _interval = 0;
+        private DateTime _last = new DateTime();
+        #endregion
+
+        #region Constant variables
+        private static readonly string REG_VERSION   = "Version";
+        private static readonly string REG_LASTCHECK = "LastCheckUpdate";
+        #endregion
+    }
+}
---
+++ CubePdfTests/Settings/UpdateCheckerTester.cs
@@ -0,0 +1,84 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Settings/UpdateCheckerTester.cs
+///
+/// Copyright (c) 2013 CubeSoft, Inc. All rights reserved.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see < http://www.gnu.org/licenses/ >.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CubePdfTests.Settings
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// UpdateCheckerTester
+    /// 
+    /// <summary>
+    /// UpdateChecker クラスのテストをするためのクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestFixture]
+    public class UpdateCheckerTester
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// TestNotify
+        /// 
+        /// <summary>
+        /// サーバへインストールが完了した事を通知するためのテストを行います。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void TestNotify()
+        {
+            try
+            {
+                var checker = new CubePdf.Settings.UpdateChecker();
+                checker.ProductName = "cubepdf";
+                checker.Version = "1.0.0RC4";
+                checker.Notify();
+            }
+            catch (Exception err)
+            {
+                Assert.Fail(err.ToString());
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// TestGetResponse
+        /// 
+        /// <summary>
+        /// サーバからバージョンアップに関するレスポンスを取得するための
+        /// テストです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void TestGetResponse()
+        {
+            var checker = new CubePdf.Settings.UpdateChecker();
+            checker.ProductName = "cubepdf";
+            checker.Version = "0.9.1β";
+            var response = checker.GetResponse();
+            Assert.NotNull(response);
+        }
+    }
+}
