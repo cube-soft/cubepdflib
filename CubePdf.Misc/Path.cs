88241414fe929e53f54e5b16f41e001e5321258f
clown <tt.clown@gmail.com>
2013-07-05 01:34:32 +0900

    正しいパスかどうかを判別するメソッドを追加

--- CubePdf.Misc/Path.cs
+++ CubePdf.Misc/Path.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Path.cs
+///
+/// Copyright (c) 2013 CubeSoft, Inc. All rights reserved.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see < http://www.gnu.org/licenses/ >.
+///
+/* ------------------------------------------------------------------------- */
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -17,6 +37,65 @@ namespace CubePdf.Misc
     {
         /* ----------------------------------------------------------------- */
         ///
+        /// IsValid
+        /// 
+        /// <summary>
+        /// 引数に指定された文字列がパスとして使用できるものであるかどうか
+        /// 判別します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static bool IsValid(string path)
+        {
+            char[] invalids = { '/', '*', '"', '<', '>', '|' };
+            if (string.IsNullOrEmpty(path)) return false;
+
+            var inactivated = false;
+            for (int i = 0; i < path.Length; ++i)
+            {
+                switch (path[i])
+                {
+                    case ':':  // ドライブ指定 (C:\foo) かどうかを判定
+                        int first = inactivated ? 5 : 1;
+                        if (!(i == first && char.IsLetter(path[i - 1]) && i + 1 < path.Length && path[i + 1] == '\\')) return false;
+                        break;
+                    case '?':  // 拡張機能の不活性化指定 (\\?\) かどうかを判定
+                        if (i + 1 < path.Length && i == 2 && path[i - 1] == '\\' && path[i - 2] == '\\' && path[i + 1] == '\\') inactivated = true;
+                        else return false;
+                        break;
+                    default:
+                        if (System.Array.IndexOf(invalids, path[i]) >= 0) return false;
+                        break;
+                }
+            }
+            return inactivated || (path[path.Length - 1] != '.' && path[path.Length - 1] != ' ');
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// IsValid
+        /// 
+        /// <summary>
+        /// 引数に指定された文字列がファイル名として使用できるものであるか
+        /// どうか判別します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static bool IsValidFilename(string path)
+        {
+            char[] invalids = { '/', '*', '"', '<', '>', '|', '?', ':', '\\' };
+            if (string.IsNullOrEmpty(path)) return false;
+            if (path[path.Length - 1] == '.' || path[path.Length - 1] == ' ') return false;
+
+            foreach (var c in path)
+            {
+                if (System.Array.IndexOf(invalids, c) >= 0) return false;
+            }
+            return true;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
         /// Normalize
         /// 
         /// <summary>
@@ -28,62 +107,36 @@ namespace CubePdf.Misc
         public static string Normalize(string path, char replaced)
         {
             char[] invalids = { '/', '*', '"', '<', '>', '|' };
+            char[] all = { '/', '*', '"', '<', '>', '|', ':', '?', '\\' };
+            if (System.Array.IndexOf(all, replaced) >= 0) throw new ArgumentException();
 
             var inactivated = false;
             var buffer = new System.Text.StringBuilder();
             for (int i = 0; i < path.Length; ++i)
             {
-                var current = path[i];
-                foreach (var c in invalids)
+                var c = (System.Array.IndexOf(invalids, path[i]) >= 0) ? replaced : path[i];
+                switch (c)
                 {
-                    if (current == c) current = replaced;
+                    case ':':  // ドライブ指定 (C:\foo) かどうかを判定
+                        int first = inactivated ? 5 : 1;
+                        if (!(i == first && char.IsLetter(path[i - 1]) && i + 1 < path.Length && path[i + 1] == '\\')) c = replaced;
+                        break;
+                    case '?':  // 拡張機能の不活性化指定 (\\?\) かどうかを判定
+                        if (i + 1 < path.Length && i == 2 && path[i - 1] == '\\' && path[i - 2] == '\\' && path[i + 1] == '\\') inactivated = true;
+                        else c = replaced;
+                        break;
+                    case '\\': // ホスト名指定 (\\server\foo) 以外の \ 記号の重複は取り除く
+                        if (i > 1 && path[i - 1] == '\\') continue;
+                        break;
+                    default:
+                        break;
                 }
-
-                // ドライブ指定としての : 記号かどうかを判定する
-                // 拡張機能の不活性化が指定されていない場合、C:\hoge
-                // 指定されている場合は \\?\C:\hoge の位置でのみ : 記号が出現する事を許す
-                if (current == ':')
-                {
-                    int first = inactivated ? 5 : 1;
-                    if (i == first && char.IsLetter(path[i - 1]) && i + 1 < path.Length && path[i + 1] == '\\')
-                    {
-                        // ドライブ指定なので : 記号を保持
-                    }
-                    else current = replaced;
-                }
-
-                // 拡張機能の不活性化指定である \\?\ のみ許す
-                if (current == '?')
-                {
-                    if (i + 1 < path.Length && i == 2 && path[i - 1] == '\\' && path[i - 2] == '\\' && path[i + 1] == '\\')
-                    {
-                        inactivated = true;
-                    }
-                    else current = replaced;
-                }
-
-                // ホスト名指定 (最初の \\server\hoge) 以外の \ 記号の重複は取り除く
-                if (current == '\\')
-                {
-                    if (i > 1 && path[i - 1] == '\\') continue;
-                }
-
-                buffer.Append(current);
+                buffer.Append(c);
             }
 
             // 末尾の . 記号や半角スペースは取り除く
             // ただし、拡張機能の不活性化が指定されている場合は保持する
-            if (!inactivated)
-            {
-                int n = 0;
-                for (int i = buffer.Length - 1; i >= 0; --i)
-                {
-                    if (buffer[i] == '.' || buffer[i] == ' ') ++n;
-                    else break;
-                }
-                if (n > 0) buffer.Remove(buffer.Length - n, n);
-            }
-
+            if (!inactivated) TrimRight(buffer);
             return buffer.ToString();
         }
 
@@ -101,19 +154,31 @@ namespace CubePdf.Misc
         public static string NormalizeFilename(string filename, char replaced)
         {
             char[] invalids = { '/', '*', '"', '<', '>', '|', '?', ':', '\\' };
+            if (System.Array.IndexOf(invalids, replaced) >= 0) throw new ArgumentException();
 
             var buffer = new System.Text.StringBuilder();
-            for (int i = 0; i < filename.Length; ++i)
+            foreach (var c in filename)
             {
-                char current = filename[i];
-                foreach (char c in invalids)
-                {
-                    if (current == c) current = replaced;
-                }
-                buffer.Append(current);
+                var normalized = (System.Array.IndexOf(invalids, c) >= 0) ? replaced : c;
+                buffer.Append(normalized);
             }
+            TrimRight(buffer);
+            return buffer.ToString();
+        }
 
-            // 末尾の . 記号や半角スペースは取り除く
+        #region Private methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// TrimRight
+        /// 
+        /// <summary>
+        /// 末尾に存在する . 記号や半角スペースを取り除きます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private static int TrimRight(System.Text.StringBuilder buffer)
+        {
             int n = 0;
             for (int i = buffer.Length - 1; i >= 0; --i)
             {
@@ -121,8 +186,9 @@ namespace CubePdf.Misc
                 else break;
             }
             if (n > 0) buffer.Remove(buffer.Length - n, n);
-
-            return buffer.ToString();
+            return n;
         }
+
+        #endregion
     }
 }
--- CubePdfTests/Misc/PathTester.cs
+++ CubePdfTests/Misc/PathTester.cs
@@ -45,18 +45,47 @@ namespace CubePdfTests.Misc
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        [TestCase(@"C:\foo\bar\bas.txt",            @"C:\foo\bar\bas.txt",            true)]
-        [TestCase(@"\\server\foo\bar\bas.txt",      @"\\server\foo\bar\bas.txt",      true)]
-        [TestCase(@"C:\foo\bar\bas______.txt",      @"C:\foo\bar\bas/*?<>|.txt",     false)]
-        [TestCase(@"C:\foo\bar\bas_.txt",           @"C:\foo\bar\bas:.txt",          false)]
-        [TestCase(@"C:\foo\bar\bas_\hoge.txt",      @"C:\foo\bar\bas:\hoge.txt",     false)]
-        [TestCase(@"c:\foo\bar\bas.txt",            @"c:\foo\\bar\bas.txt",           true)]
-        [TestCase(@"\\?\c:\foo\bar\bas.txt",        @"\\?\c:\foo\bar\bas.txt",        true)]
-        [TestCase(@"C:\foo\bar\bas.txt",            @"C:\foo\bar\bas.txt . ... ",    false)]
-        [TestCase(@"\\?\C:\foo\bar\bas.txt . ... ", @"\\?\C:\foo\bar\bas.txt . ... ", true)]
+        [TestCase(@"C:\foo\bar\bas.txt",        @"C:\foo\bar\bas.txt",         true)]
+        [TestCase(@"\\server\foo\bar\bas.txt",  @"\\server\foo\bar\bas.txt",   true)]
+        [TestCase(@"C:\foo\bar\bas______.txt",  @"C:\foo\bar\bas/*?<>|.txt",  false)]
+        [TestCase(@"C:\foo\bar\bas_.txt",       @"C:\foo\bar\bas:.txt",       false)]
+        [TestCase(@"C:\foo\bar\bas_\hoge.txt",  @"C:\foo\bar\bas:\hoge.txt",  false)]
+        [TestCase(@"c:\foo\bar\bas.txt",        @"c:\foo\\bar\bas.txt",        true)]
+        [TestCase(@"\\?\c:\foo\bar\bas.txt",    @"\\?\c:\foo\bar\bas.txt",     true)]
+        [TestCase(@"C:\foo\bar\bas.txt",        @"C:\foo\bar\bas.txt . ... ", false)]
+        [TestCase(@"\\?\C:\foo\bar.txt . ... ", @"\\?\C:\foo\bar.txt . ... ",  true)]
+        [TestCase(@"__foo\bar\bas__",           @"??foo\bar\bas??",           false)]
+        [TestCase(@"__foo\bar\bas__",           @"::foo\bar\bas::",           false)]
         public void TestNormalizePath(string expected, string src, bool valid)
         {
-            Assert.AreEqual(expected, CubePdf.Misc.Path.Normalize(src, '_'));
+            try
+            {
+                Assert.AreEqual(valid, CubePdf.Misc.Path.IsValid(src));
+                Assert.AreEqual(expected, CubePdf.Misc.Path.Normalize(src, '_'));
+            }
+            catch (Exception err) { Assert.Fail(err.ToString()); }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// TestNormalizePathException
+        /// 
+        /// <summary>
+        /// 置換用文字にパス名に使用できない文字を指定した時のテストを
+        /// 行います。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void TestNormalizePathException()
+        {
+            try
+            {
+                CubePdf.Misc.Path.Normalize(@"c:\foo\bar", ':');
+                Assert.Fail("never reached");
+            }
+            catch (ArgumentException /* err */) { Assert.Pass(); }
+            catch (Exception err) { Assert.Fail(err.ToString()); }
         }
 
         /* ----------------------------------------------------------------- */
@@ -68,16 +97,43 @@ namespace CubePdfTests.Misc
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        [TestCase(@"foo.txt",                      @"foo.txt",                      true)]
-        [TestCase(@"symbols!#$%&'()=~`{}@;,..txt", @"symbols!#$%&'()=~`{}@;,..txt", true)]
-        [TestCase(@"C__foo_bar_bas.txt",           @"C:\foo\bar\bas.txt",          false)]
-        [TestCase(@"__server_foo_bar_bas.txt",     @"\\server\foo\bar\bas.txt",    false)]
-        [TestCase(@"C__foo_bar_bas______.txt",     @"C:\foo\bar\bas/*?<>|.txt",    false)]
-        [TestCase(@"____c__foo_bar_bas.txt",       @"\\?\c:\foo\bar\bas.txt",      false)]
-        [TestCase(@"C__foo_bar_bas.txt",           @"C:\foo\bar\bas.txt . ... ",   false)]
+        [TestCase(@"foo.txt",                  @"foo.txt",                    true)]
+        [TestCase(@"s!#$%&'()=~`{}@;,..txt",   @"s!#$%&'()=~`{}@;,..txt",     true)]
+        [TestCase(@"C__foo_bar_bas.txt",       @"C:\foo\bar\bas.txt",        false)]
+        [TestCase(@"__server_foo_bar_bas.txt", @"\\server\foo\bar\bas.txt",  false)]
+        [TestCase(@"C__foo_bar_bas______.txt", @"C:\foo\bar\bas/*?<>|.txt",  false)]
+        [TestCase(@"____c__foo_bar_bas.txt",   @"\\?\c:\foo\bar\bas.txt",    false)]
+        [TestCase(@"C__foo_bar_bas.txt",       @"C:\foo\bar\bas.txt . ... ", false)]
         public void TestNormalizeFilename(string expected, string src, bool valid)
         {
-            Assert.AreEqual(expected, CubePdf.Misc.Path.NormalizeFilename(src, '_'));
+            try
+            {
+                Assert.AreEqual(valid, CubePdf.Misc.Path.IsValidFilename(src));
+                Assert.AreEqual(expected, CubePdf.Misc.Path.NormalizeFilename(src, '_'));
+            }
+            catch (Exception err) { Assert.Fail(err.ToString()); }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// TestNormalizeFilenameException
+        /// 
+        /// <summary>
+        /// 置換用文字にファイル名に使用できない文字を指定した時のテストを
+        /// 行います。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void TestNormalizeFilenameException()
+        {
+            try
+            {
+                CubePdf.Misc.Path.NormalizeFilename(@"c:\foo\bar", '\\');
+                Assert.Fail("never reached");
+            }
+            catch (ArgumentException /* err */) { Assert.Pass(); }
+            catch (Exception err) { Assert.Fail(err.ToString()); }
         }
     }
 }
