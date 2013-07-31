7c93e121b792ae6cf542e2639b6f675d9b9c312c
clown <tt.clown@gmail.com>
2013-07-31 18:38:44 +0900

    結合時の諸問題を修正
    - 各ページの回転指定が無視される問題
    - 上書き保存時に例外が送出される問題
    - PDF バージョンの指定が無視される問題

--- CubePdf.Editing/PageBinder.cs
+++ CubePdf.Editing/PageBinder.cs
@@ -52,163 +52,6 @@ namespace CubePdf.Editing
 
         #endregion
 
-        #region Public Methods
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Reset
-        /// 
-        /// <summary>
-        /// PageBinder クラスを初期状態にリセットします。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        public void Reset()
-        {
-            _metadata = new CubePdf.Data.Metadata();
-            _encrypt = new CubePdf.Data.Encryption();
-            _pages.Clear();
-        }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Save
-        /// 
-        /// <summary>
-        /// 現在メンバ変数が保持している、メタデータ、暗号化に関する情報、
-        /// 各ページ情報に基づいた PDF ファイルを指定されたパスに保存
-        /// します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        public void Save(string path)
-        {
-            try
-            {
-                string tmp = Path.GetTempPath() + Path.GetFileName(path);
-                var doc = new PdfCopyFields(new FileStream(tmp, FileMode.Create));
-                var readers = new Dictionary<string, iTextSharp.text.pdf.PdfReader>();
-                int pagenum = 1;
-                foreach (var page in _pages)
-                {
-                    if (!readers.ContainsKey(page.FilePath))
-                    {
-                        var item = page.Password.Length > 0 ?
-                            new iTextSharp.text.pdf.PdfReader(page.FilePath, System.Text.Encoding.UTF8.GetBytes(page.Password)) :
-                            new iTextSharp.text.pdf.PdfReader(page.FilePath);
-                        readers.Add(page.FilePath, item);
-                    }
-                    var reader = readers[page.FilePath];
-                    doc.AddDocument(reader, page.PageNumber.ToString());
-                    AddBookmarks(reader, page.PageNumber, pagenum++);
-                }
-                doc.Close();
-                foreach (var reader in readers.Values) reader.Close();
-                readers.Clear();
-
-                EditMetadata(tmp, path);
-                File.Delete(tmp);
-            }
-            catch (iTextSharp.text.pdf.BadPasswordException err)
-            {
-                throw new CubePdf.Data.EncryptionException(err.Message, err);
-            }
-        }
-
-        #endregion
-
-        #region Other methods
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// EditMetadata
-        /// 
-        /// <summary>
-        /// PdfStamperを用いて、Metadataなどを付与した新たなpdfを作ります。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        private void EditMetadata(string src, string dest)
-        {
-            var reader = new PdfReader(src);
-            var stamper = new PdfStamper(reader, new FileStream(dest, FileMode.Create));
-
-            var info = reader.Info;
-            info.Add("Title", _metadata.Title);
-            info.Add("Subject", _metadata.Subtitle);
-            info.Add("Keywords", _metadata.Keywords);
-            info.Add("Creator", _metadata.Creator);
-            info.Add("Author", _metadata.Author);
-            stamper.MoreInfo = info;
-
-            if (_encrypt.IsEnabled && _encrypt.OwnerPassword.Length > 0)
-            {
-                var method = Translator.ToIText(_encrypt.Method);
-                var permission = Translator.ToIText(_encrypt.Permission);
-                var userpassword = _encrypt.IsUserPasswordEnabled ? _encrypt.UserPassword : "";
-                stamper.Writer.SetEncryption(method, userpassword, _encrypt.OwnerPassword, permission);
-            }
-            stamper.Writer.Outlines = _bookmarks;
-            stamper.Writer.PdfVersion = _metadata.Version.Minor.ToString()[0];
-
-            stamper.Close();
-            reader.Close();
-        }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// CopyAnnotations
-        /// 
-        /// <summary>
-        /// 元の PDF ファイルにある注釈をコピーします。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        private void CopyAnnotations(PdfWriter dest, PdfReader src, int pagenum)
-        {
-            var page = src.GetPageN(pagenum);
-            var rotate = src.GetPageRotation(pagenum);
-            if (page == null) return;
-
-            var annots = page.GetAsArray(PdfName.ANNOTS);
-            if (annots == null) return;
-
-            for (int i = 0; i < annots.Size; i++)
-            {
-                var dic = PdfReader.GetPdfObject(annots[i]) as PdfDictionary;
-                if (dic != null)
-                {
-                    var annotation = new PdfAnnotation(dest, null);
-                    foreach (var item in dic) annotation.Put(item.Key, PdfReader.GetPdfObject(item.Value));
-                    dest.AddAnnotation(annotation);
-                }
-            }
-        }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// AddBookmarks
-        /// 
-        /// <summary>
-        /// 元の PDF ファイルにあるしおりを_bookmarksに追加します。
-        /// 実際にしおりをPDFに追加するにはOutlinesプロパティに代入が必要
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        private void AddBookmarks(PdfReader src, int srcpage, int destpage)
-        {
-            var bookmarks = SimpleBookmark.GetBookmark(src);
-            if (bookmarks == null) return;
-
-            SimpleBookmark.ShiftPageNumbers(bookmarks, destpage - srcpage, null);
-            foreach (var bm in bookmarks)
-            {
-                if (bm["Page"].ToString().Contains(destpage.ToString() + " FitH")) { _bookmarks.Add(bm); }
-            }
-        }
-
-        #endregion
-
         #region Properties
 
         /* ----------------------------------------------------------------- */
@@ -257,10 +100,191 @@ namespace CubePdf.Editing
 
         #endregion
 
+        #region Public Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Reset
+        /// 
+        /// <summary>
+        /// PageBinder クラスを初期状態にリセットします。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Reset()
+        {
+            _metadata = new CubePdf.Data.Metadata();
+            _encrypt = new CubePdf.Data.Encryption();
+            _pages.Clear();
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Save
+        /// 
+        /// <summary>
+        /// 現在メンバ変数が保持している、メタデータ、暗号化に関する情報、
+        /// 各ページ情報に基づいた PDF ファイルを指定されたパスに保存
+        /// します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Save(string path)
+        {
+            try
+            {
+                var tmp = Path.GetTempFileName();
+                File.Delete(tmp);
+                BindPages(tmp);
+
+                using (var reader = new PdfReader(tmp))
+                using (var writer = new PdfStamper(reader, new FileStream(path, FileMode.Create)))
+                {
+                    RotatePages(reader);
+                    AddMetadata(reader, writer);
+                    AddSecurity(writer);
+                    writer.Writer.Outlines = _bookmarks;
+                }
+                File.Delete(tmp);
+            }
+            catch (iTextSharp.text.pdf.BadPasswordException err)
+            {
+                throw new CubePdf.Data.EncryptionException(err.Message, err);
+            }
+        }
+
+        #endregion
+
+        #region Other methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// BindPages
+        /// 
+        /// <summary>
+        /// 指定された各ページを結合し、新たな PDF ファイルを生成します。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// 注釈等を含めて完全にページ内容をコピーするためにいったん
+        /// PdfCopyFields クラスを用いて全ページを結合します。回転等の
+        /// 付随的な処理は生成された PDF に対して改めて行います。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void BindPages(string dest)
+        {
+            var readers = new Dictionary<string, iTextSharp.text.pdf.PdfReader>();
+            var writer = new PdfCopyFields(new FileStream(dest, FileMode.Create), _metadata.Version.Minor.ToString()[0]);
+            var pagenum = 1;
+
+            _bookmarks.Clear();
+            foreach (var page in _pages)
+            {
+                if (!readers.ContainsKey(page.FilePath))
+                {
+                    var item = page.Password.Length > 0 ?
+                        new iTextSharp.text.pdf.PdfReader(page.FilePath, System.Text.Encoding.UTF8.GetBytes(page.Password)) :
+                        new iTextSharp.text.pdf.PdfReader(page.FilePath);
+                    readers.Add(page.FilePath, item);
+                }
+                var reader = readers[page.FilePath];
+                writer.AddDocument(reader, page.PageNumber.ToString());
+                AddBookmarks(reader, page.PageNumber, pagenum++);
+            }
+            writer.Close();
+            foreach (var reader in readers.Values) reader.Close();
+            readers.Clear();
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// RotatePages
+        /// 
+        /// <summary>
+        /// 各ページを回転します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void RotatePages(PdfReader reader)
+        {
+            for (int i = 0; i < reader.NumberOfPages; ++i)
+            {
+                if (i >= _pages.Count) break;
+                var rot = reader.GetPageRotation(i + 1);
+                var dic = reader.GetPageN(i + 1);
+                if (rot != _pages[i].Rotation) dic.Put(PdfName.ROTATE, new PdfNumber(_pages[i].Rotation));
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// AddBookmarks
+        /// 
+        /// <summary>
+        /// 元の PDF ファイルにあるしおりを_bookmarksに追加します。
+        /// 実際にしおりをPDFに追加するにはOutlinesプロパティに代入が必要
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void AddBookmarks(PdfReader reader, int srcpage, int destpage)
+        {
+            var bookmarks = SimpleBookmark.GetBookmark(reader);
+            if (bookmarks == null) return;
+
+            SimpleBookmark.ShiftPageNumbers(bookmarks, destpage - srcpage, null);
+            foreach (var bm in bookmarks)
+            {
+                if (bm["Page"].ToString().Contains(destpage.ToString() + " FitH")) _bookmarks.Add(bm);
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// AddMetadata
+        /// 
+        /// <summary>
+        /// タイトル、著者名等の各種メタデータを追加します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void AddMetadata(PdfReader reader, PdfStamper writer)
+        {
+            var info = reader.Info;
+            info.Add("Title",    _metadata.Title);
+            info.Add("Subject",  _metadata.Subtitle);
+            info.Add("Keywords", _metadata.Keywords);
+            info.Add("Creator",  _metadata.Creator);
+            info.Add("Author",   _metadata.Author);
+            writer.MoreInfo = info;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// AddSecurity
+        /// 
+        /// <summary>
+        /// 各種セキュリティ情報を付加します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void AddSecurity(PdfStamper writer)
+        {
+            if (_encrypt.IsEnabled && _encrypt.OwnerPassword.Length > 0)
+            {
+                var method = Translator.ToIText(_encrypt.Method);
+                var permission = Translator.ToIText(_encrypt.Permission);
+                var userpassword = _encrypt.IsUserPasswordEnabled ? _encrypt.UserPassword : "";
+                writer.Writer.SetEncryption(method, userpassword, _encrypt.OwnerPassword, permission);
+            }
+        }
+
+        #endregion
+
         #region Variables
         private CubePdf.Data.IMetadata _metadata = new CubePdf.Data.Metadata();
         private CubePdf.Data.IEncryption _encrypt = new CubePdf.Data.Encryption();
-        private List<CubePdf.Data.IPage> _pages = new List<CubePdf.Data.IPage>();
+        private IList<CubePdf.Data.IPage> _pages = new List<CubePdf.Data.IPage>();
         private IList<Dictionary<String, Object>> _bookmarks = new List<Dictionary<String, Object>>();
         #endregion
     }
