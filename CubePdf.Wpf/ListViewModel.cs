2f012befb7044942edaa7e0b620a4be3e9456b14
clown <tt.clown@gmail.com>
2014-03-04 23:22:33 +0900

    サムネイルサイズの算出方法を修正

--- CubePdf.Wpf/ListViewModel.cs
+++ CubePdf.Wpf/ListViewModel.cs
@@ -1272,13 +1272,9 @@ namespace CubePdf.Wpf
         /// 
         /// <remarks>
         /// TODO:
-        /// - margin の値は、現在の CubePDF Utility に基づいた値である。
-        ///   項目ごとの Margin の値（現在は 3 に設定）によって変化するので、
-        ///   それらの値に応じて変更するような形に修正する。
-        /// - 現時点でも、幅一杯にギリギリ収まる場合とギリギリ収まらない場合
-        ///   (ほぼ 1 個分の余白が右側に空く) とで、うまく整合性が取れて
-        ///   いない。現状では、ギリギリ収まる場合を優先している（ギリギリ
-        ///   収まらない場合のサムネイル表示がおかしくなる）。
+        /// - margin の値は、テストを基に現状でもっともずれの少ない算出
+        ///   方法を用いている。実際には、項目の余白、および枠線の値を
+        ///   基に算出する必要があると思われる。
         /// </remarks>
         /// 
         /* ----------------------------------------------------------------- */
@@ -1291,15 +1287,21 @@ namespace CubePdf.Wpf
             {
                 var scroll = VisualHelper.FindVisualChild<System.Windows.Controls.ScrollViewer>(View);
                 if (scroll == null) return all;
-
-                var margin = 3.0;
+                
                 var width  = (double)Math.Max(ItemWidth, 1);
                 var height = (double)Math.Max(ItemHeight, 1);
+                var margin = 1.5 * width / 100.0; // NOTE: empirically
                 var column = (int)(_view.ActualWidth / (width + margin));
                 var row    = (int)(_view.ActualHeight / (height + margin));
                 var index  = (int)(scroll.VerticalOffset / height) * column;
                 if (index < 0 || index > _pages.Count) return all;
-                return new KeyValuePair<int, int>(index, Math.Min(index + column * (row + 2), _pages.Count - 1));
+
+                var dest = new KeyValuePair<int, int>(index, Math.Min(index + column * (row + 2), _pages.Count - 1));
+                Debug.WriteLine(string.Format("col:{0}({1}/{2}), row:{3}({4}/{5}) => [{6}-{7}]",
+                    column, _view.ActualWidth,  width  + margin,
+                    row,    _view.ActualHeight, height + margin,
+                    dest.Key, dest.Value));
+                return dest;
             }
             catch (Exception err)
             {
