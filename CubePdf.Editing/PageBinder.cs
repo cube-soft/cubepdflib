03adfe5d17f6a27bac26a7514da5ed72b260c7c9
ichikawa <i.shu0408+cube@gmail.com>
2014-09-10 13:32:51 +0900

    PageBinder.csを微修正

--- CubePdf.Editing/PageBinder.cs
+++ CubePdf.Editing/PageBinder.cs
@@ -171,8 +171,8 @@ namespace CubePdf.Editing
                 {
                     AddMetadata(reader, writer);
                     AddSecurity(writer);
-                    // PDFversion > 1.5でのみFullCompressionは実行可
-                    if (Metadata.Version.Minor > 5) writer.SetFullCompression();
+                    // PDFversion >= 1.5でのみFullCompressionは実行可
+                    if (Metadata.Version.Minor >= 5) writer.SetFullCompression();
                     writer.Writer.Outlines = _bookmarks;
                 }
                 File.Delete(tmp);
