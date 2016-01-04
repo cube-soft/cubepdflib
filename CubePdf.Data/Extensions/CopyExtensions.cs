99a46c01b11e535a0b65c6c65ad5fc9b51622380
clown <tt.clown@gmail.com>
2016-01-04 15:28:28 +0900

    CopyExtensions を追加

---
+++ CubePdf.Data/Extensions/CopyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CubePdf.Data.Extensions
+{
+    public static class CopyExtensions
+    {
+        public static T Copy<T>(this T target)
+        {
+            var formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, target);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
